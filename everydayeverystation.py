3aa224f5659636428ba46d25d7fb279fddaa31b5
ic666 <73983686+ic666@users.noreply.github.com>
2021-01-24 23:10:07 +0800

    Delete everydayeverystation.py

--- everydayeverystation.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-
-df = pd.read_json('http://ods.railway.gov.tw/tra-ods-web/ods/download/dataResource/8ae4cabf6973990e0169947ed32454b9')
-df.to_excel('everydayeverystation.xlsx')
