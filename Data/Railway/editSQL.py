6ba04ace72c44282614060b3c63758ce35a2eee3
Dieark <57029719+Dieark@users.noreply.github.com>
2021-03-02 13:22:13 +0800

    建立一個專門編輯SQL的py檔

--- Data/Railway/editSQL.py
+++ Data/Railway/editSQL.py
@@ -4,20 +4,20 @@ db=pymysql.connect(host="localhost",user="root",password="Dieark20000115",databa
 cursor=db.cursor()
 
 listname=[
-# "豐原(順行)",
-# "豐原(逆行)",
-# "太原(順行)",
-# "太原(逆行)",
-# "臺中(順行)",
-# "臺中(逆行)",
-# "潭子(順行)",
-# "潭子(逆行)",
-# "新烏日(順行)",
-# "新烏日(逆行)",
-# "彰化(順行)",
-# "彰化(逆行)",
-# "員林(順行)",
-# "員林(逆行)",
+"豐原(順行)",
+"豐原(逆行)",
+"太原(順行)",
+"太原(逆行)",
+"臺中(順行)",
+"臺中(逆行)",
+"潭子(順行)",
+"潭子(逆行)",
+"新烏日(順行)",
+"新烏日(逆行)",
+"彰化(順行)",
+"彰化(逆行)",
+"員林(順行)",
+"員林(逆行)",
 "基隆(順行)",
 "基隆(逆行)",
 "汐止(順行)",
@@ -47,7 +47,8 @@ listname=[
 "高雄(順行)",
 "高雄(逆行)"]
 for listn in listname:
-    sql_create="CREATE TABLE `railway`.`"+listn+"` ( `id` VARCHAR(3) NOT NULL , `date` VARCHAR(10) NOT NULL , `time` VARCHAR(10) NOT NULL , `num` VARCHAR(100) NOT NULL , `destination` VARCHAR(5) NOT NULL , `ontime` VARCHAR(10) NOT NULL ) ENGINE = MyISAM;"
-    cursor.execute(sql_create)
+    sql_add="ALTER TABLE `"+listn+"` ADD type varchar(10);"
+    # sql_create="CREATE TABLE `railway`.`"+listn+"` ( `id` VARCHAR(3) NOT NULL , `date` VARCHAR(10) NOT NULL , `time` VARCHAR(10) NOT NULL , `num` VARCHAR(100) NOT NULL , `destination` VARCHAR(5) NOT NULL , `ontime` VARCHAR(10) NOT NULL ) ENGINE = MyISAM;"
+    cursor.execute(sql_add)
     db.commit()
 print("success")
